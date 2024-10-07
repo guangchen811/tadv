620ac67e652301afaffa1e7964bab2caed1691bd
Hao Chen <hao.chen@tu-berlin.de>
2024-10-07 13:43:57 +0200

    First Test

---
+++ tests/jenga/test_numerical_corruptions.py
@@ -0,0 +1,15 @@
+from jenga.corruptions.numerical import GaussianNoise
+import numpy as np
+import pandas as pd
+
+
+def test_gaussian_noise():
+    df = pd.DataFrame({
+        'a': np.arange(10),
+        'b': np.arange(10),
+        'c': np.arange(10),
+    })
+    gaussian_noise = GaussianNoise(fraction=0.1, column='a')
+    corrupted_df = gaussian_noise.transform(df)
+    assert df.shape == corrupted_df.shape
+    assert not df.equals(corrupted_df)
