4b91c89d8165c266350c8f7ff5d522c5bf3c17aa
Hao Chen <hao.chen@tu-berlin.de>
2024-10-07 16:35:41 +0200

    add utils

---
+++ cadv_exploration/utils/__init__.py
@@ -0,0 +1,3 @@
+from cadv_exploration.utils._utils import get_project_root
+
+__all__ = ["get_project_root"]
---
+++ cadv_exploration/utils/_utils.py
@@ -0,0 +1,11 @@
+"""
+Some useful utils for the project
+"""
+from pathlib import Path
+
+import numpy
+
+
+def get_project_root() -> Path:
+    """Returns the project root folder."""
+    return Path(__file__).parent.parent.parent
