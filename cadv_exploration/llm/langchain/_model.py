ad39d9623c8967ddfa81b1acd02f8da6b0c8a3c3
Hao Chen <hao.chen@tu-berlin.de>
2024-10-14 11:45:28 +0200

    impl LangChainCADV

--- cadv_exploration/llm/langchain/_model.py
+++ cadv_exploration/llm/langchain/_model.py
@@ -1,12 +1,73 @@
 import os
-from cadv_exploration.utils import get_project_root
-import dotenv
 
-dotenv.load_dotenv(get_project_root() / ".env")
-
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import (
+    CommaSeparatedListOutputParser,
+    JsonOutputParser,
+)
+
+
+# from cadv_exploration.utils import get_project_root
+# from cadv_exploration.utils import load_dotenv
+
+from cadv_exploration.llm.langchain._prompt import (
+    SYSTEM_TASK_DESCRIPTION,
+    RELEVENT_COLUMN_TARGET_PROMPT,
+    EXPECTATION_EXTRACTION_PROMPT,
+)
+
+from cadv_exploration.llm._tasks import DVTask
 
 
 class LangChainCADV:
-    def __init__(self):
-        self.llm = ChatOpenAI()
+    def __init__(self, model: str = None):
+        if model is None:
+            self.llm = ChatOpenAI()
+        else:
+            self.llm = ChatOpenAI(model=model)
+
+    def build_prompt(self, task: DVTask) -> ChatPromptTemplate:
+        if task == DVTask.RELEVENT_COLUMN_TARGET:
+            return ChatPromptTemplate.from_messages(
+                [
+                    ("system", SYSTEM_TASK_DESCRIPTION),
+                    ("human", RELEVENT_COLUMN_TARGET_PROMPT),
+                ],
+            )
+        elif task == DVTask.EXPECTATION_EXTRACTION:
+            return ChatPromptTemplate.from_messages(
+                [
+                    ("system", SYSTEM_TASK_DESCRIPTION),
+                    ("human", EXPECTATION_EXTRACTION_PROMPT),
+                ],
+            )
+        elif task == DVTask.CHECK_FORMULATION:
+            raise NotImplementedError("Check formulation task not yet implemented")
+
+    def _build_single_chain(self, task: DVTask):
+        if task == DVTask.RELEVENT_COLUMN_TARGET:
+            prompt = self.build_prompt(task)
+            parser = CommaSeparatedListOutputParser()
+            single_chain = prompt | parser
+        elif task == DVTask.EXPECTATION_EXTRACTION:
+            prompt = self.build_prompt(task)
+            parser = JsonOutputParser()
+            single_chain = prompt | parser
+        elif task == DVTask.CHECK_FORMULATION:
+            raise NotImplementedError("Check formulation task not yet implemented")
+        else:
+            raise ValueError(f"Unknown task {task}")
+        return single_chain
+
+    def build_whole_chain(self):
+        relevant_column_target_chain = self._build_single_chain(
+            DVTask.RELEVENT_COLUMN_TARGET
+        )
+        expectation_extraction_chain = self._build_single_chain(
+            DVTask.EXPECTATION_EXTRACTION
+        )
+        whole_chain = {
+            "relevant_columns": relevant_column_target_chain
+        } | expectation_extraction_chain
+        return whole_chain
