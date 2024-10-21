1726267c902ed9112dedaac5a5f24e7937c118af
Hao Chen <hao.chen@tu-berlin.de>
2024-10-21 13:58:47 +0200

    add an example to run ipynb file locally

--- cadv_exploration/runtime_environments/kaggle/executor.py
+++ cadv_exploration/runtime_environments/kaggle/executor.py
@@ -1,26 +1,24 @@
 import os
 import subprocess
+from pathlib import Path
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
 
 
 class KaggleExecutor:
-    def run(self, local_data_path, script_path, is_competition):
-        # Run the Kaggle environment in Docker
-        if is_competition:
-            docker_data_path = "/kaggle/input/"
+    def __init__(self):
+        pass
+
+    def run(self, local_data_path: Path, script_path: Path, output_path: Path, is_competition: bool):
+        command_prefix = self._prepare_prefix(local_data_path, script_path, output_path, is_competition)
+        script_file_name = os.path.basename(script_path)
+        # script_file_path =
+        if str(script_path).endswith(".py"):
+            command = self._compsoe_py_command(command_prefix, script_file_name)
+        elif str(script_path).endswith(".ipynb"):
+            command = self._compsoe_ipynb_command(command_prefix, script_file_name)
         else:
-            docker_data_path = f"/kaggle/input/{os.path.basename(os.path.abspath(local_data_path))}/"
-        command = [
-            "docker",
-            "run",
-            "--rm",
-            "-v",
-            f"{os.path.join(os.path.abspath(local_data_path), 'files/')}:{docker_data_path}",
-            "-v",
-            f"{os.path.abspath(script_path)}:/kaggle/script.py",
-            "kaggle-env/python:1.0.0",
-            "python",
-            "/kaggle/script.py",
-        ]
+            raise ValueError("Invalid script type")
 
         result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         if result.returncode == 0:
@@ -29,5 +27,39 @@ class KaggleExecutor:
         else:
             print("Execution failed:")
             print(result.stderr.decode())
-
         return result
+
+    def _compsoe_py_command(self, command_prefix: list, script_file_name: str) -> list:
+        # Run the Kaggle environment in Docker
+        command = command_prefix + ["python", f"/kaggle/script/{script_file_name}"]
+        return command
+
+    def _compsoe_ipynb_command(self, command_prefix: list, script_file_name: str) -> list:
+        # https://nbconvert.readthedocs.io/en/latest/execute_api.html# module-nbconvert.preprocessors
+        command = command_prefix + ["jupyter", "nbconvert", "--to", "notebook", "--output",
+                                    "/kaggle/output/output.ipynb", "--execute", f"/kaggle/script/{script_file_name}"]
+        return command
+
+    @staticmethod
+    def _prepare_prefix(local_data_path: Path, script_path: Path, output_path: Path, is_competition):
+        if is_competition:
+            docker_data_path = f"/kaggle/input/{local_data_path.name}/"
+        else:
+            docker_data_path = f"/kaggle/input/{local_data_path.name}/"
+        if str(script_path).endswith(".py"):
+            script_type = "py"
+        elif str(script_path).endswith(".ipynb"):
+            script_type = "ipynb"
+        command = [
+            "docker",
+            "run",
+            "--rm",
+            "-v",
+            f"{local_data_path / 'files'}:{docker_data_path}",
+            "-v",
+            f"{script_path.parent}:/kaggle/script/",
+            "-v",
+            f"{output_path}:/kaggle/output/",
+            "kaggle-env/python:1.0.0",
+        ]
+        return command
--- tests/runtime/kaggle/test_runnable.py
+++ tests/runtime/kaggle/test_runnable.py
@@ -1,7 +1,9 @@
 import subprocess
 from pathlib import Path
-
+import os
 import pytest
+import nbformat
+import pandas as pd
 
 from cadv_exploration.loader._py_file import load_py_files
 from cadv_exploration.runtime_environments.kaggle import KaggleExecutor
@@ -37,7 +39,7 @@ def test_runnable_on_personal_dataset():
     )
 
     script_paths = load_py_files(script_dir, return_files=False)
-    result = executor.run(data_path.parent, script_paths[0], is_competition=False)
+    result = executor.run_py_script(data_path.parent, script_paths[0], is_competition=False)
     assert result is not None
 
 
@@ -55,5 +57,81 @@ def test_runnable_on_competition_dataset():
         project_root / "data" / "santander-value-prediction-challenge" / "kernels_py"
     )
     script_paths = load_py_files(script_dir, return_files=False)
-    result = executor.run(data_path.parent, script_paths[0], is_competition=True)
+    result = executor.run_py_script(data_path.parent, script_paths[0], is_competition=True)
     assert result is not None
+
+
+@pytest.mark.skipif(not docker_exists(), reason="Docker is not available")
+def test_runnable_on_personal_dataset_with_ipynb():
+    executor = KaggleExecutor()
+    project_root = get_project_root()
+    data_path = Path(
+        project_root
+        / "data"
+        / "prasad22"
+        / "healthcare-dataset"
+        / "files"
+    )
+    script_dir = Path(
+        project_root / "data" / "prasad22" / "healthcare-dataset" / "kernels_ipynb"
+    )
+
+    script_paths = load_py_files(script_dir, return_files=False)
+    result = executor.run(data_path.parent, script_paths[0], is_competition=False)
+    assert result is not None
+
+
+@pytest.mark.skipif(not docker_exists(), reason="Docker is not available")
+def test_runnable_on_competition_dataset_with_ipynb():
+    executor = KaggleExecutor()
+    project_root = get_project_root()
+    data_path = Path(
+        project_root
+        / "data"
+        / "santander-value-prediction-challenge"
+        / "files"
+    )
+    script_dir = Path(
+        project_root / "data" / "santander-value-prediction-challenge" / "kernels_ipynb"
+    )
+    script_paths = load_py_files(script_dir, return_files=False)
+    result = executor.run(data_path.parent, script_paths[1], is_competition=True)
+    assert result is not None
+
+
+def test_runnable_on_small_test_dataset_with_ipynb(tmp_path):
+    executor = KaggleExecutor()
+    project_root = get_project_root()
+    data_path = Path(
+        project_root / "tests" / "data"
+    )
+    script_dir = Path(
+        project_root
+        / "tests"
+        / "data"
+        / "kernel_ipynb"
+        / "example_notebook.ipynb"
+    )
+    output_path = tmp_path / "output"
+    _ = executor.run(data_path, script_dir, output_path, is_competition=True)
+
+    assert os.path.exists(output_path)
+    assert len(list(output_path.iterdir())) == 1
+    assert (output_path / "output.ipynb").exists()
+    with open(output_path / "output.ipynb", "r") as f:
+        nb = nbformat.read(f, as_version=4)
+        assert len(nb.cells) == 5
+        assert nb.cells[0].cell_type == "markdown"
+        assert nb.cells[1].cell_type == "code"
+        assert nb.cells[2]['outputs'][0][
+                   'text'] == "Index(['UserName', 'Identifier', 'FirstName', 'LastName'], dtype='object')\n"
+    submission_file = script_dir.parent / "submission.csv"
+    assert submission_file.exists()
+    try:
+        with open(submission_file, "r") as f:
+            submission = pd.read_csv(f)
+            assert 'FullName' in submission.columns
+    finally:
+        # Clean up: ensure that the submission.csv file is deleted
+        if submission_file.exists():
+            os.remove(submission_file)
