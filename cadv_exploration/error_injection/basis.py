c750a55e1a27045d3ba1ce5422a30816f96bf533
Hao Chen <hao.chen@tu-berlin.de>
2024-11-04 17:21:16 +0100

    refine error injection method

--- cadv_exploration/error_injection/basis.py
+++ cadv_exploration/error_injection/basis.py
@@ -17,6 +17,7 @@ class TabularCorruption(DataCorruption):
     def __init__(self, columns=None, **kwargs):
         super().__init__(**kwargs)
         self.columns = columns
+        self.fraction = 1.0
 
     @abstractmethod
     def transform(self, dataframe):
@@ -30,3 +31,30 @@ class TabularCorruption(DataCorruption):
     def validate_data(dataframe):
         if not isinstance(dataframe, pd.DataFrame):
             raise TypeError("Input data must be a pandas DataFrame.")
+
+    def sample_rows(self, data):
+        if self.fraction == 1.0:
+            rows = data.index
+        # Completely At Random
+        elif self.sampling.endswith('CAR'):
+            rows = np.random.permutation(data.index)[:int(len(data) * self.fraction)]
+        elif self.sampling.endswith('NAR') or self.sampling.endswith('AR'):
+            n_values_to_discard = int(len(data) * min(self.fraction, 1.0))
+            perc_lower_start = np.random.randint(0, len(data) - n_values_to_discard)
+            perc_idx = range(perc_lower_start, perc_lower_start + n_values_to_discard)
+
+            # Not At Random
+            if self.sampling.endswith('NAR'):
+                # pick a random percentile of values in this column
+                rows = data[self.column].sort_values().iloc[perc_idx].index
+
+            # At Random
+            elif self.sampling.endswith('AR'):
+                depends_on_col = np.random.choice(list(set(data.columns) - {self.column}))
+                # pick a random percentile of values in other column
+                rows = data[depends_on_col].sort_values().iloc[perc_idx].index
+
+        else:
+            ValueError(f"sampling type '{self.sampling}' not recognized")
+
+        return rows
--- cadv_exploration/error_injection/corrupts/categorical_value_missing.py
+++ cadv_exploration/error_injection/corrupts/categorical_value_missing.py
@@ -1,6 +1,4 @@
 import numpy as np
-import pandas as pd
-from numpy.ma.core import masked
 
 from cadv_exploration.error_injection.basis import TabularCorruption
 
