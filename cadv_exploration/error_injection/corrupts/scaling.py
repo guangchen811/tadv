484c04710b90e51094814de6f799e3d997bed116
Hao Chen <hao.chen@tu-berlin.de>
2024-11-04 17:22:23 +0100

    scaling error injection

---
+++ cadv_exploration/error_injection/corrupts/scaling.py
@@ -0,0 +1,18 @@
+from cadv_exploration.error_injection.basis import TabularCorruption
+import numpy as np
+
+
+class Scaling(TabularCorruption):
+
+    def identify_columns(self, dataframe):
+        raise NotImplementedError("Scaling corruption does not require column identification.")
+
+    def transform(self, dataframe: object) -> object:
+        df = dataframe.copy(deep=True)
+
+        scale_factor = np.random.choice([10, 100, 1000])
+
+        rows = self.sample_rows(dataframe)
+        df.loc[rows, self.columns] *= scale_factor
+
+        return df
