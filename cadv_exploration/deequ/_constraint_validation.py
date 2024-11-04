87e7d7f3a5b3af1d8f626427383744160f354b2d
Hao Chen <hao.chen@tu-berlin.de>
2024-11-04 17:20:51 +0100

    add new function

--- cadv_exploration/deequ/_constraint_validation.py
+++ cadv_exploration/deequ/_constraint_validation.py
@@ -3,6 +3,16 @@ from pydeequ.verification import *
 
 
 def validate_suggestions(spark, spark_df, check):
-    checkResult = VerificationSuite(spark).onData(spark_df).addCheck(check).run()
-    checkResult_df = VerificationResult.checkResultsAsDataFrame(spark, checkResult)
-    return checkResult_df
+    check_result = VerificationSuite(spark).onData(spark_df).addCheck(check).run()
+    check_result_df = VerificationResult.checkResultsAsDataFrame(spark, check_result)
+    return check_result_df
+
+
+def apply_checks_from_strings(check, check_strings, spark, spark_df):
+    for check_str in check_strings:
+        exec(f"check.addConstraint(check.{check_str})")
+    check_result = VerificationSuite(spark).onData(spark_df).addCheck(check).run()
+    check_result = VerificationResult.checkResultsAsDataFrame(
+        spark, check_result
+    ).collect()
+    return check_result
