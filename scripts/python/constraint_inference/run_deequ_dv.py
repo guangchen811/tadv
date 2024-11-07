0417d9686dbff77d1be20644bf26ad7d7304dadd
Hao Chen <hao.chen@tu-berlin.de>
2024-11-07 16:26:29 +0100

    refactor

--- scripts/python/constraint_inference/run_deequ_dv.py
+++ scripts/python/constraint_inference/run_deequ_dv.py
@@ -32,62 +32,85 @@ def main():
     args = parser.parse_args()
 
     project_root = get_project_root()
-    original_file_path = (
+    original_train_file_path = (
             project_root
             / "data"
             / "prasad22"
             / "healthcare-dataset"
             / "broken_files"
-            / "original"
+            / "original_train"
             / "healthcare_dataset.csv"
     )
-    before_broken_file_path = original_file_path.parent.parent / "before_broken" / "healthcare_dataset.csv"
-    after_broken_file_path = original_file_path.parent.parent / "after_broken" / "healthcare_dataset.csv"
+    original_validation_file_path = original_train_file_path.parent.parent / "original_validation" / "healthcare_dataset.csv"
+    pre_corruption_file_path = original_train_file_path.parent.parent / "pre_corruption" / "healthcare_dataset.csv"
+    post_corruption_file_path = original_train_file_path.parent.parent / "post_corruption" / "healthcare_dataset.csv"
 
-    original_df = load_csv(original_file_path)
-    before_broken_df = load_csv(before_broken_file_path)
-    after_broken_df = load_csv(after_broken_file_path)
+    original_train_df = load_csv(original_train_file_path)
+    original_validation_df = load_csv(original_validation_file_path)
+    pre_corruption_df = load_csv(pre_corruption_file_path)
+    post_corruption_df = load_csv(post_corruption_file_path)
 
-    spark_original_df, spark_original = spark_df_from_pandas_df(original_df)
-    spark_before_broken_df, spark_before_broken = spark_df_from_pandas_df(before_broken_df)
-    spark_after_broken_df, spark_after_broken = spark_df_from_pandas_df(after_broken_df)
+    spark_original_train_df, spark_original_train = spark_df_from_pandas_df(original_train_df)
+    spark_original_validation_df, spark_original_validation = spark_df_from_pandas_df(original_validation_df)
+    spark_pre_corruption_df, spark_pre_corruption = spark_df_from_pandas_df(pre_corruption_df)
+    spark_post_corruption_df, spark_post_corruption = spark_df_from_pandas_df(post_corruption_df)
 
-    suggestion = get_suggestion_for_spark_df(spark_original, spark_original_df)
-    code_list_for_constraints = [item["code_for_constraint"] for item in suggestion if
-                                 "Room Number" not in item["code_for_constraint"]]
+    suggestion = get_suggestion_for_spark_df(spark_original_train, spark_original_train_df)
+    code_list_for_constraints = [item["code_for_constraint"] for item in suggestion]
 
+    check_original = Check(spark_original_validation, CheckLevel.Warning, "Check for original data")
+    check_result_on_original_validation_df = apply_checks_from_strings(check_original, code_list_for_constraints,
+                                                                       spark_original_validation,
+                                                                       spark_original_validation_df)
+    status_on_original_validation_df = [item['constraint_status'] if
+                                        item is not None else None for item in check_result_on_original_validation_df]
+    success_on_original_validation_df = status_on_original_validation_df.count("Success")
+    failure_check_on_original_validation_df = [code_list_for_constraints[i] for i in
+                                               range(len(check_result_on_original_validation_df)) if
+                                               check_result_on_original_validation_df[i] is not None and
+                                               check_result_on_original_validation_df[i][
+                                                   'constraint_status'] == 'Failure']
+    failure_check_output_on_original_validation_df = "\n".join(failure_check_on_original_validation_df)
+    failure_check_on_original_validation_df = [code_list_for_constraints[i] for i in
+                                               range(len(code_list_for_constraints)) if
+                                               check_result_on_original_validation_df[i] == None]
+    grammarly_failure_check_output_on_original_validation_df = "\n".join(failure_check_on_original_validation_df)
+    logger.info(f"Check result on original data: {check_result_on_original_validation_df}")
+    logger.info(
+        f"Success on original data: {success_on_original_validation_df} / {len(status_on_original_validation_df)}")
+    logger.info(f"Failure check on original data: {failure_check_output_on_original_validation_df}")
+    logger.info(f"Grammarly failure check on original data: {grammarly_failure_check_output_on_original_validation_df}")
+
+    code_list_for_constraints = [code_list_for_constraints[i] for i in range(len(code_list_for_constraints)) if
+                                 status_on_original_validation_df[i] == "Success"]
     logger.info(f"Code list for constraints: {code_list_for_constraints}")
 
-    check_before_broken = Check(spark_before_broken, CheckLevel.Warning, "Check for data before broken")
-    check_after_broken = Check(spark_after_broken, CheckLevel.Warning, "Check for data after broken")
 
-    check_result_on_before_broken_df = apply_checks_from_strings(check_before_broken, code_list_for_constraints,
-                                                                 spark_before_broken, spark_before_broken_df)
-    status_on_before_broken_df = [item['constraint_status'] for item in check_result_on_before_broken_df if item is not None]
-    success_on_before_broken_df = status_on_before_broken_df.count("Success")
-    failure_check_on_before_broken_df = [item.constraint for item in check_result_on_before_broken_df if
-                                         item['constraint_status'] == 'Failure']
-    failure_check_output_on_before_broken_df = "\n".join(failure_check_on_before_broken_df)
-    logger.info(f"Check result on before broken data: {check_result_on_before_broken_df}")
-    logger.info(f"Success on before broken data: {success_on_before_broken_df} / {len(status_on_before_broken_df)}")
-    logger.info(f"Failure check on before broken data: {failure_check_output_on_before_broken_df}")
+    # Validate the constraints on the before broken data
+    check_pre_corruption = Check(spark_pre_corruption, CheckLevel.Warning, "Check for data before corruption")
+    validate_on_df(check_pre_corruption, code_list_for_constraints, spark_pre_corruption, spark_pre_corruption_df)
 
-    check_result_on_after_broken_df = apply_checks_from_strings(check_after_broken, code_list_for_constraints,
-                                                                spark_after_broken, spark_after_broken_df)
-    status_on_after_broken_df = [item['constraint_status'] for item in check_result_on_after_broken_df if item is not None]
-    success_on_after_broken_df = status_on_after_broken_df.count("Success")
-    failure_check_on_after_broken_df = [item.constraint for item in check_result_on_after_broken_df if
-                                        item['constraint_status'] == 'Failure']
-    failure_check_output_on_after_broken_df = "\n".join(failure_check_on_after_broken_df)
-    logger.info(f"Check result on after broken data: {check_result_on_after_broken_df}")
-    logger.info(f"Success on after broken data: {success_on_after_broken_df} / {len(status_on_after_broken_df)}")
-    logger.info(f"Failure check on after broken data: {failure_check_output_on_after_broken_df}")
+    # Validate the constraints on the after broken data
+    check_post_corruption = Check(spark_post_corruption, CheckLevel.Warning, "Check for data after corruption")
+    validate_on_df(check_post_corruption, code_list_for_constraints, spark_post_corruption, spark_post_corruption_df)
 
-    spark_before_broken.sparkContext._gateway.shutdown_callback_server()
-    spark_after_broken.sparkContext._gateway.shutdown_callback_server()
-    spark_before_broken.stop()
-    spark_after_broken.stop()
+    spark_pre_corruption.sparkContext._gateway.shutdown_callback_server()
+    spark_post_corruption.sparkContext._gateway.shutdown_callback_server()
+    spark_pre_corruption.stop()
+    spark_post_corruption.stop()
 
+def validate_on_df(check, code_list_for_constraints, spark, spark_df):
+    check_result_on_post_corruption_df = apply_checks_from_strings(check, code_list_for_constraints,
+                                                                   spark, spark_df)
+    status_on_post_corruption_df = [item['constraint_status'] if
+                                    item is not None else None for item in check_result_on_post_corruption_df]
+    success_on_post_corruption_df = status_on_post_corruption_df.count("Success")
+    failure_check_on_post_corruption_df = [item.constraint for item in check_result_on_post_corruption_df if
+                                           item['constraint_status'] == 'Failure']
+    failure_check_output_on_post_corruption_df = "\n".join(failure_check_on_post_corruption_df)
+    logger.info(f"Check result on after broken data: {check_result_on_post_corruption_df}")
+    logger.info(f"Success on after broken data: {success_on_post_corruption_df} / {len(status_on_post_corruption_df)}")
+    logger.info(f"Failure check on after broken data: {failure_check_output_on_post_corruption_df}")
 
 if __name__ == "__main__":
     main()
--- scripts/python/constraint_inference/run_langchain_cadv.py
+++ scripts/python/constraint_inference/run_langchain_cadv.py
@@ -36,27 +36,30 @@ def main():
     logging.info(f"Script ID: {args.script_id}")
 
     project_root = get_project_root()
-    original_file_path = (
+    original_train_file_path = (
             project_root
             / "data"
             / "prasad22"
             / "healthcare-dataset"
             / "broken_files"
-            / "original"
+            / "original_train"
             / "healthcare_dataset.csv"
     )
-    before_broken_file_path = original_file_path.parent.parent / "before_broken" / "healthcare_dataset.csv"
-    after_broken_file_path = original_file_path.parent.parent / "after_broken" / "healthcare_dataset.csv"
+    original_validation_file_path = original_train_file_path.parent.parent / "original_validation" / "healthcare_dataset.csv"
+    pre_corruption_file_path = original_train_file_path.parent.parent / "pre_corruption" / "healthcare_dataset.csv"
+    post_corruption_file_path = original_train_file_path.parent.parent / "post_corruption" / "healthcare_dataset.csv"
 
-    original_df = load_csv(original_file_path)
-    before_broken_df = load_csv(before_broken_file_path)
-    after_broken_df = load_csv(after_broken_file_path)
+    original_train_df = load_csv(original_train_file_path)
+    original_validation_df = load_csv(original_validation_file_path)
+    pre_corruption_df = load_csv(pre_corruption_file_path)
+    post_corruption_df = load_csv(post_corruption_file_path)
 
-    spark_original_df, spark_original = spark_df_from_pandas_df(original_df)
-    spark_before_broken_df, spark_before_broken = spark_df_from_pandas_df(before_broken_df)
-    spark_after_broken_df, spark_after_broken = spark_df_from_pandas_df(after_broken_df)
+    spark_original_train_df, spark_original_train = spark_df_from_pandas_df(original_train_df)
+    spark_original_validation_df, spark_original_validation = spark_df_from_pandas_df(original_validation_df)
+    spark_pre_corruption_df, spark_pre_corruption = spark_df_from_pandas_df(pre_corruption_df)
+    spark_post_corruption_df, spark_post_corruption = spark_df_from_pandas_df(post_corruption_df)
 
-    column_desc = spark_df_to_column_desc(spark_original, spark_original_df)
+    column_desc = spark_df_to_column_desc(spark_original_train, spark_original_train_df)
 
     dir_path = project_root / "data" / "prasad22" / "healthcare-dataset" / "kernels_py"
     scripts = load_py_files(dir_path)
@@ -80,65 +83,57 @@ def main():
     logger.info(f"Code list for constraints: {code_list_for_constraints}")
 
     # Validate the constraints on the original data to see if they are grammarly correct
-    check_original = Check(spark_original, CheckLevel.Warning, "Check for original data")
-    check_result_on_original_df = apply_checks_from_strings(check_original, code_list_for_constraints, spark_original,
-                                                            spark_original_df)
-    status_on_original_df = [item['constraint_status'] if
-                             item is not None else None for item in check_result_on_original_df]
-    success_on_original_df = status_on_original_df.count("Success")
-    failure_check_on_original_df = [code_list_for_constraints[i] for i in range(len(check_result_on_original_df)) if
-                                    check_result_on_original_df[i] is not None and
-                                    check_result_on_original_df[i]['constraint_status'] == 'Failure']
+    check_original = Check(spark_original_validation, CheckLevel.Warning, "Check for original data")
+    check_result_on_original_validation_df = apply_checks_from_strings(check_original, code_list_for_constraints,
+                                                                       spark_original_validation,
+                                                                       spark_original_validation_df)
+    status_on_original_validation_df = [item['constraint_status'] if
+                                        item is not None else None for item in check_result_on_original_validation_df]
+    success_on_original_df = status_on_original_validation_df.count("Success")
+    failure_check_on_original_df = [code_list_for_constraints[i] for i in
+                                    range(len(check_result_on_original_validation_df)) if
+                                    check_result_on_original_validation_df[i] is not None and
+                                    check_result_on_original_validation_df[i]['constraint_status'] == 'Failure']
     failure_check_output_on_original_df = "\n".join(failure_check_on_original_df)
     failure_check_on_original_df = [code_list_for_constraints[i] for i in
                                     range(len(code_list_for_constraints)) if
-                                    check_result_on_original_df[i] == None]
+                                    check_result_on_original_validation_df[i] == None]
     grammarly_failure_check_output_on_original_df = "\n".join(failure_check_on_original_df)
-    logger.info(f"Check result on original data: {check_result_on_original_df}")
-    logger.info(f"Success on original data: {success_on_original_df} / {len(status_on_original_df)}")
+    logger.info(f"Check result on original validation data: {check_result_on_original_validation_df}")
+    logger.info(f"Success on original data: {success_on_original_df} / {len(status_on_original_validation_df)}")
     logger.info(f"Failure check on original data: {failure_check_output_on_original_df}")
     logger.info(f"Grammarly failure check on original data: {grammarly_failure_check_output_on_original_df}")
 
     # remove the constraints that are not grammarly correct
     code_list_for_constraints = [code_list_for_constraints[i] for i in range(len(code_list_for_constraints)) if
-                                 status_on_original_df[i] == "Success"]
+                                 status_on_original_validation_df[i] == "Success"]
 
     # Validate the constraints on the before broken data
-    check_before_broken = Check(spark_before_broken, CheckLevel.Warning, "Check for data before broken")
-    check_after_broken = Check(spark_after_broken, CheckLevel.Warning, "Check for data after broken")
-
-    check_result_on_before_broken_df = apply_checks_from_strings(check_before_broken, code_list_for_constraints,
-                                                                 spark_before_broken, spark_before_broken_df)
-    status_on_before_broken_df = [item['constraint_status'] if
-                                  item is not None else None for item in check_result_on_before_broken_df]
-
-    success_on_before_broken_df = status_on_before_broken_df.count("Success")
-    failure_check_on_before_broken_df = [item.constraint for item in check_result_on_before_broken_df if
-                                         item['constraint_status'] == 'Failure']
-    failure_check_output_on_before_broken_df = "\n".join(failure_check_on_before_broken_df)
-    logger.info(f"Check result on before broken data: {check_result_on_before_broken_df}")
-    logger.info(f"Success on before broken data: {success_on_before_broken_df} / {len(status_on_before_broken_df)}")
-    logger.info(f"Failure check on before broken data: {failure_check_output_on_before_broken_df}")
+    check_pre_corruption = Check(spark_pre_corruption, CheckLevel.Warning, "Check for data before corruption")
+    validate_on_df(check_pre_corruption, code_list_for_constraints, spark_pre_corruption, spark_pre_corruption_df)
 
     # Validate the constraints on the after broken data
-    check_result_on_after_broken_df = apply_checks_from_strings(check_after_broken, code_list_for_constraints,
-                                                                spark_after_broken, spark_after_broken_df)
-    status_on_after_broken_df = [item['constraint_status'] if
-                                 item is not None else None for item in check_result_on_after_broken_df]
+    check_post_corruption = Check(spark_post_corruption, CheckLevel.Warning, "Check for data after corruption")
+    validate_on_df(check_post_corruption, code_list_for_constraints, spark_post_corruption, spark_post_corruption_df)
 
-    success_on_after_broken_df = status_on_after_broken_df.count("Success")
-    failure_check_on_after_broken_df = [item.constraint for item in check_result_on_after_broken_df if
-                                        item['constraint_status'] == 'Failure']
-    failure_check_output_on_after_broken_df = "\n".join(failure_check_on_after_broken_df)
+    spark_pre_corruption.sparkContext._gateway.shutdown_callback_server()
+    spark_post_corruption.sparkContext._gateway.shutdown_callback_server()
+    spark_pre_corruption.stop()
+    spark_post_corruption.stop()
 
-    logger.info(f"Check result on after broken data: {check_result_on_after_broken_df}")
-    logger.info(f"Success on after broken data: {success_on_after_broken_df} / {len(status_on_after_broken_df)}")
-    logger.info(f"Failure check on after broken data: {failure_check_output_on_after_broken_df}")
 
-    spark_before_broken.sparkContext._gateway.shutdown_callback_server()
-    spark_after_broken.sparkContext._gateway.shutdown_callback_server()
-    spark_before_broken.stop()
-    spark_after_broken.stop()
+def validate_on_df(check, code_list_for_constraints, spark, spark_df):
+    check_result_on_post_corruption_df = apply_checks_from_strings(check, code_list_for_constraints,
+                                                                   spark, spark_df)
+    status_on_post_corruption_df = [item['constraint_status'] if
+                                    item is not None else None for item in check_result_on_post_corruption_df]
+    success_on_post_corruption_df = status_on_post_corruption_df.count("Success")
+    failure_check_on_post_corruption_df = [item.constraint for item in check_result_on_post_corruption_df if
+                                           item['constraint_status'] == 'Failure']
+    failure_check_output_on_post_corruption_df = "\n".join(failure_check_on_post_corruption_df)
+    logger.info(f"Check result on after broken data: {check_result_on_post_corruption_df}")
+    logger.info(f"Success on after broken data: {success_on_post_corruption_df} / {len(status_on_post_corruption_df)}")
+    logger.info(f"Failure check on after broken data: {failure_check_output_on_post_corruption_df}")
 
 
 if __name__ == "__main__":
