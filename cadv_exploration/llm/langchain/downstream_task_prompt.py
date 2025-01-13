24ce9baba10f71376ee90b0774b12f1290023ee7
Hao Chen <hao.chen@tu-berlin.de>
2025-01-13 23:02:54 +0100

    add task prompt

--- cadv_exploration/llm/langchain/downstream_task_prompt.py
+++ cadv_exploration/llm/langchain/downstream_task_prompt.py
@@ -7,3 +7,19 @@ SQL_QUERY_TASK_DESCRIPTION = cleandoc(
     """The code is written for SQL Query task. The code is expected to run on a database and return the results.
 you are asked to generate constraints on the upcoming *test* data to ensure that the code can run without any errors and the results are meaningful."""
 )
+
+CD_TASK_DESCRIPTION = cleandoc(
+    """The code is designed to process the current version of the data and should also work seamlessly with future datasets. Your task is to write constraints that validate abnormalities in the data.
+
+For categorical attributes, your goal is to provide a comprehensive and accurate list of all possible categories within the given domain.
+	•	Expand the list to include all valid and expected categories, even if they are not present in the current version of the data. This ensures that the constraints avoid false alerts and account for valid categories in upcoming data.
+	•	Ensure the list is complete, capturing all common and relevant categories.
+	•	Use consistent formatting (e.g., lowercase) if specified.
+	•	Avoid duplicates and irrelevant entries.
+The categorical value here includes all the attributes that you can complete with common sense like colors, countries, states, seasons, etc.
+Try your best to provide a comprehensive list of categories that can be used to validate the data.
+
+Example:
+If the domain involves colors and the input includes "red" and "blue", expand the output to include all possible categories:
+"red", "blue", "green", "yellow", "purple", "orange", "pink", "brown", "black", "white", "gray"
+    """)
