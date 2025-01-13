1726f37edba1410a0ddb7e0adf02eebb05b11b32
Hao Chen <hao.chen@tu-berlin.de>
2025-01-13 23:03:12 +0100

    add toy example

---
+++ data/toy_example/scripts_ml/example.py
@@ -0,0 +1,25 @@
+class ColumnDetectionTask:
+
+    def code_type(self):
+        return "python"
+
+    @property
+    def original_code(self):
+        return """
+df = pd.read_csv("s3://datalake/latest/hospitalisations.csv")
+df['cost_smoothed'] = np.log(df['cost'])
+df['admission_day'].fillna(df['discharge_day'])
+df['duration'] = df['discharge_day'] - df['admission_day']
+categorical_cols = ['diagnosis', 'insurance']
+for col in categorical_cols:
+df[col] = pd.get_dummies(df[col], dummy_na=True)
+features = df[categorical_cols + ['duration', 'cost_smoothed']]
+labels = label_binarize(df['complications'], classes=['Y', 'N'])
+model = sklearn.tree.DecisionTreeClassifier()
+model.fit(train_features, train_labels)
+deploy_to_production(model)
+"""
+
+    def required_columns(self):
+        # Ground truth for columns used in the ML pipeline
+        return ["diagnosis", "admission_day", "discharge_day", "insurance", "cost", "complications"]
---
+++ data/toy_example/scripts_sql/example.py
@@ -0,0 +1,22 @@
+class ColumnDetectionTask:
+
+    def code_type(self):
+        return "python"
+
+    @property
+    def original_code(self):
+        return """
+nonsensitive_df = duckdb.sql("SELECT * EXCLUDE ssn, gender, race
+FROM 's3://datalake/latest/hospitalisations.csv'").df()
+hosp_df = nonsensitive_df.dropna()
+strokes_total = duckdb.sql("SELECT COUNT(*) FROM hosp_df
+WHERE diagnosis = 'stroke'").fetch()
+strokes_for_rare_bloodtypes = duckdb.sql("SELECT COUNT(*)
+FROM hosp_df WHERE diagnosis = 'stroke'
+AND bloodtype IN ('AB negative', 'B negative')").fetch()
+generate_report(strokes_total, strokes_for_rare_bloodtypes)
+"""
+
+    def required_columns(self):
+        # Ground truth for columns used in the ML pipeline
+        return ["diagnosis", "bloodtype"]
