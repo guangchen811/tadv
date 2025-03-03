689f821e142f367f43bff59a524dff7e2c4ec3b8
Hao Chen <hao.chen@tu-berlin.de>
2025-03-03 11:55:02 +0100

    fix typo

--- cadv_exploration/project_manager/abstract.py
+++ cadv_exploration/project_manager/abstract.py
@@ -8,20 +8,20 @@ class AbstractProjectManager(ABC):
     def __init__(self, dataset_name, downstream_task_types=None):
         super().__init__()
         self.project_root = get_project_root()
-        self._avaliable_datasets = [item.name for item in self.project_data_path.iterdir()]
+        self._available_datasets = [item.name for item in self.project_data_path.iterdir()]
         self.dataset_path = self.project_data_path / dataset_name
         if not self.dataset_path.exists():
             raise ValueError(
-                f"Dataset {dataset_name} is not available in the project data path. Available datasets are: {self._avaliable_datasets}")
+                f"Dataset {dataset_name} is not available in the project data path. Available datasets are: {self._available_datasets}")
         if self.scripts_path.exists():
-            self._avaliable_tasks = [item.name for item in self.scripts_path.iterdir()]
+            self._available_tasks = [item.name for item in self.scripts_path.iterdir()]
         else:
-            self._avaliable_tasks = []
-        if downstream_task_types is not None and downstream_task_types not in self._avaliable_tasks:
+            self._available_tasks = []
+        if downstream_task_types is not None and downstream_task_types not in self._available_tasks:
             raise ValueError(
-                f"Downstream task type {downstream_task_types} is not available in the scripts path. Available tasks are: {self._avaliable_tasks}")
+                f"Downstream task type {downstream_task_types} is not available in the scripts path. Available tasks are: {self._available_tasks}")
         elif downstream_task_types is None:
-            self.downstream_task_types = self._avaliable_tasks
+            self.downstream_task_types = self._available_tasks
             self.downstream_task_type_path = [self.scripts_path / item for item in self.downstream_task_types]
         elif isinstance(downstream_task_types, str):
             self.downstream_task_types = [downstream_task_types]
@@ -31,7 +31,7 @@ class AbstractProjectManager(ABC):
             self.downstream_task_type_path = [self.scripts_path / item for item in self.downstream_task_types]
         else:
             raise ValueError("Invalid downstream task types")
-        
+
     @property
     def project_data_path(self):
         """The default data path is the project root/data. Override this method if you want to change the default data path."""
