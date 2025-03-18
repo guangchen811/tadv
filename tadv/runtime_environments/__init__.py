cedaa5c767f882215031e74af210c8c43fd91d58
Hao Chen <hao.chen@tu-berlin.de>
2025-03-18 10:23:48 +0100

    remove kaggle environment

--- tadv/runtime_environments/__init__.py
+++ tadv/runtime_environments/__init__.py
@@ -1,5 +1,4 @@
 from tadv.runtime_environments.duckdb.executor import DuckDBExecutor
-from tadv.runtime_environments.kaggle.executor import KaggleExecutor
 from tadv.runtime_environments.python.executor import PythonExecutor
 
-__all__ = ["KaggleExecutor", "DuckDBExecutor", "PythonExecutor"]
+__all__ = ["DuckDBExecutor", "PythonExecutor"]
