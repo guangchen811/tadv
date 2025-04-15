81aa11d72ba33f414f7608121cdc75bfd69e7b1e
Hao Chen <hao.chen@tu-berlin.de>
2025-04-15 11:25:36 +0200

    refactoring to fix the bug

--- tadv/data_models/expectation_config.py
+++ tadv/data_models/expectation_config.py
@@ -1,17 +1,12 @@
 from dataclasses import dataclass, field
-from typing import List, Optional, Dict, Union
+from typing import List, Optional, Dict
 
-import yaml
-
-
-@dataclass
-class Example:
-    column: str
-    value_set: List[Union[str, int, float]]
+import oyaml as yaml
+import pandas as pd
 
 
 @dataclass
-class Parameter:
+class GXParameter:
     type: str
     description: str
 
@@ -21,27 +16,30 @@ class ExpectationConfig:
     Name: str
     URL: str
     Description: str
-    Args: Dict[str, Parameter]
-    Examples: Dict[str, List[Example]]
-    Other_Parameters: Dict[str, Parameter]
+    Args: Dict[str, GXParameter]
+    SampleData: pd.DataFrame
+    Examples: Dict[str, str]
+    Other_Parameters: Dict[str, GXParameter]
     Data_quality_issues: Optional[str] = None
     Related_Expectations: Optional[List[str]] = field(default_factory=list)
 
     @staticmethod
-    def from_yaml_file(filepath: str) -> "ExpectationConfig":
+    def from_yaml_file(filepath: str):
         def parse_parameter_dict(d):
-            return {k: Parameter(**v) for k, v in d.items()}
+            return {k: GXParameter(**v) for k, v in d.items()}
 
         with open(filepath, "r") as f:
             data = yaml.safe_load(f)
 
+        example_data = data["Examples"]["Sample data"]
+        example_df = pd.DataFrame({ex["column"]: ex["value_set"] for ex in example_data})
         return ExpectationConfig(
             Name=data.get("Name"),
             URL=data.get("URL"),
             Description=data.get("Description"),
             Args=parse_parameter_dict({k: v for param in data["Args"] for k, v in param.items()}),
+            SampleData=example_df,
             Examples={
-                "Sample data": [Example(**ex) for ex in data["Examples"]["Sample data"]],
                 "Passing case": data["Examples"].get("Passing case", []),
                 "Failing case": data["Examples"].get("Failing case", [])
             },
@@ -50,18 +48,3 @@ class ExpectationConfig:
             Data_quality_issues=data.get("Data quality issues"),
             Related_Expectations=data.get("Related Expectations", [])
         )
-
-
-if __name__ == "__main__":
-    config = ExpectationConfig.from_yaml_file("expectations/expect_column_distinct_values_to_equal_set.yaml")
-    print(config.Name)
-    print(config.Args["column"].description)
-    print(config.Examples["Sample data"][0].column)
-    print(config.Examples["Sample data"][0].value_set)
-    print(config.Examples["Passing case"])
-    print(config.Examples["Failing case"])
-    print(config.Other_Parameters)
-    print(config.Data_quality_issues)
-    print(config.Related_Expectations)
-    print(config.URL)
-    print(config.Description)
