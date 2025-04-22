1b25b95cdf2525bc9695821fc61a662985b99840
Hao Chen <hao.chen@tu-berlin.de>
2025-04-22 15:43:01 +0200

    add support for gx_dialect to support wrong constraint grammars

--- tadv/dq_manager/gx/_constraint_validation.py
+++ tadv/dq_manager/gx/_constraint_validation.py
@@ -15,17 +15,39 @@ def apply_checks_from_strings_on_spark_df(spark, spark_df, code_list_for_constra
     batch_parameters = {"dataframe": spark_df}
     suite = gx.ExpectationSuite(name=suite_name)
     suite = context.suites.add(suite)
-    expectations = []
-    for code in code_list_for_constraints:
-        expectation = eval(f"gx.expectations.{code}")
-        expectations.append(expectation)
-        suite.add_expectation(expectation)
+    expectations = {}
+    for idx, code in enumerate(code_list_for_constraints):
+        try:
+            expectation = eval(f"gx.expectations.{code}")
+            suite.add_expectation(expectation)
+            expectations[idx] = expectation
+        except AttributeError as e:
+            error_message = str(e)
+            expectations[idx] = error_message
+        except Exception as e:
+            raise RuntimeError(
+                f"Error while evaluating the expectation code '{code}': {str(e)}"
+            )
     batch = batch_definition.get_batch(batch_parameters=batch_parameters)
     validation_results = batch.validate(suite)
-    ordered_results = [
-        result for expectation in expectations
-        for result in validation_results.results
-        if result.expectation_config.id == expectation.id
-    ]
+    # ordered_results = [
+    #     result for expectation in expectations
+    #     for result in validation_results.results
+    #     if result.expectation_config.id == expectation.id
+    # ]
+    ordered_results = []
+    for idx in range(len(code_list_for_constraints)):
+        if isinstance(expectations[idx], gx.expectations.expectation.Expectation):
+            for result in validation_results.results:
+                if result.expectation_config.id == expectations[idx].id:
+                    ordered_results.append(result)
+                    break
+        else:
+            ordered_results.append(
+                {
+                    "success": False,
+                    "expectation_config": {"type": "InvalidExpectation"},
+                }
+            )
     validation_results.results = ordered_results
     return validation_results
--- tests/data_quality_manager/gx_dialect/test_gx_validation.py
+++ tests/data_quality_manager/gx_dialect/test_gx_validation.py
@@ -1,3 +1,6 @@
+import time
+from pprint import pprint
+
 import pandas as pd
 
 from tadv.dq_manager import GreatExpectationsDataQualityManager
@@ -14,14 +17,43 @@ def test_gx_quality_manager():
         'ExpectColumnValuesToBeInSet(column="a", value_set=["foo", "bar", "baz"])',
         'ExpectColumnValuesToNotBeNull(column="c")',
     ]
-
+    start = time.time()
     check_result = dq_manager.validate_on_spark_df(spark, spark_df, code_list_for_constraints)
+    end = time.time()
+    print(f"Time taken for validation: {end - start} seconds")
 
     assert check_result[0]["success"] == False
     assert check_result[1]["success"] == True
     assert check_result[2]["success"] == True
     assert check_result[3]["success"] == True
     assert check_result[4]["success"] == False
+
+    spark.sparkContext._gateway.shutdown_callback_server()
+    spark.stop()
+
+
+def test_gx_quality_manager_with_incorrect_grammar():
+    dq_manager = GreatExpectationsDataQualityManager()
+    df = pd.DataFrame({"a": ["foo", "bar", "baz"], "b": [1, 2, 3], "c": [5, 6, None]})
+    spark_df, spark = dq_manager.spark_df_from_pandas_df(df)
+    code_list_for_constraints = [
+        'xpectColumnToExist(column="def")',  # grammatically incorrect
+        'ExpectColumnValueToNotBeNull(column="a")',  # grammatically incorrect
+        'ExpectCompoundColumnsToBeUnique(column_list=["b", "c"])',
+        'ExpectColumnValuesToBeInSet(column="a", value_set=["foo", "bar", "baz"])',
+        'ExpectColumnValuesToNotBeNull(column="c")',
+    ]
+    start = time.time()
+    check_result = dq_manager.validate_on_spark_df(spark, spark_df, code_list_for_constraints)
+    end = time.time()
+    print(f"Time taken for validation: {end - start} seconds")
+
+    assert check_result[0]["success"] == False  # failed due to incorrect grammar
+    assert check_result[1]["success"] == False  # failed due to incorrect grammar
+    assert check_result[2]["success"] == True
+    assert check_result[3]["success"] == True
+    assert check_result[4]["success"] == False
+    pprint(check_result)
 
     spark.sparkContext._gateway.shutdown_callback_server()
     spark.stop()
