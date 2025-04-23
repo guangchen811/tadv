a4df74f48fd98a2374a8e4c86a86ab69c011a28a
Hao Chen <hao.chen@tu-berlin.de>
2025-04-23 19:15:04 +0200

    add necessary comments

--- tadv/llm/tasks.py
+++ tadv/llm/tasks.py
@@ -2,6 +2,13 @@ from enum import Enum, auto
 
 
 class SequentialTADVTasks(Enum):
+    """
+    Enum for sequential TADV tasks.
+    sequential TADV consists of three tasks:
+    1. COLUMN_ACCESS_DETECTION: Detects the columns accessed by the downstream task.
+    2. EXPECTATION_EXTRACTION: Extracts the expectations from the downstream task code on the columns accessed.
+    3. CODE_GENERATION: Generates the constraint code based on the expectations extracted.
+    """
     COLUMN_ACCESS_DETECTION = auto()
     EXPECTATION_EXTRACTION = auto()
     CODE_GENERATION = auto()
