c9216cbac13d4ccd3563463344dac68f7253ac8b
Hao Chen <hao.chen@tu-berlin.de>
2025-05-12 15:45:35 +0200

    init ir structure

--- tadv/ir_translator/gx_expectations/base_expectation.py
+++ tadv/ir_translator/gx_expectations/base_expectation.py
@@ -1,14 +1,15 @@
-from abc import abstractmethod, ABC
 from typing import Dict, Any
 
 from tadv.ir_translator.deequ_constraints.base_constraint import DeequConstraint
 
 
-class GXExpectation(ABC):
+class GXExpectation():
     def __init__(self, expectation_type: str, kwargs: Dict[str, Any]):
         self.expectation_type = expectation_type
         self.kwargs = kwargs
 
-    @abstractmethod
     def to_deequ_constraint(self) -> 'DeequConstraint':
-        pass
+        raise NotImplementedError(
+            f"to_deequ_constraint not implemented for {self.__class__.__name__}. "
+            "Please implement this method in the subclass."
+        )
