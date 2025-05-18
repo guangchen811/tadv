6d1147f94dae009d8788fe7893a8340c0beac0fa
Hao Chen <hao.chen@tu-berlin.de>
2025-05-18 15:22:23 +0200

    bumping version to v0.0.24

--- tadv/data_models/__init__.py
+++ tadv/data_models/__init__.py
@@ -1,6 +1,4 @@
 from tadv.data_models.constraints import CodeEntry, ColumnConstraints, Constraints
-from tadv.data_models.constraints_v2 import ColumnConstraintsWithSources, ConstraintsWithSources, SourceLocation, \
-    AssumptionEntry
 from tadv.data_models.expectation_config import ExpectationConfig
 from tadv.data_models.validated_results import ValidationCodeEntry, ColumnValidationResults, \
     ValidationResults
@@ -9,10 +7,6 @@ __all__ = [
     "CodeEntry",
     "ColumnConstraints",
     "Constraints",
-    "SourceLocation",
-    "AssumptionEntry",
-    "ColumnConstraintsWithSources",
-    "ConstraintsWithSources",
     "ValidationCodeEntry",
     "ColumnValidationResults",
     "ValidationResults",
