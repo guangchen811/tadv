0fe750537ff3a76cba2f31c6d81e5e637aa77a0e
Hao Chen <hao.chen@tu-berlin.de>
2025-06-03 10:44:58 +0200

    add spark_df_to_column_desc_dict

--- tadv/inspector/deequ/deequ_inspector_manager.py
+++ tadv/inspector/deequ/deequ_inspector_manager.py
@@ -7,6 +7,11 @@ from tadv.inspector.abstract_inspector_manager import AbstractInspectorManager
 class DeequInspectorManager(AbstractInspectorManager):
 
     def spark_df_to_column_desc(self, spark_df, spark):
+        result_dict = self.spark_df_to_column_desc_dict(spark, spark_df)
+        yaml_string = yaml.dump(result_dict, default_flow_style=False, sort_keys=False)
+        return yaml_string
+
+    def spark_df_to_column_desc_dict(self, spark, spark_df):
         dq_manager = DeequDataQualityManager()
         result = dq_manager.profile_on_spark_df(spark, spark_df)
         column_names = list(result.profiles.keys())
@@ -15,8 +20,7 @@ class DeequInspectorManager(AbstractInspectorManager):
             column_profile = result.profiles[column_name]
             column_profile_dict = self._profile_to_dict(column_profile)
             result_dict[column_name] = column_profile_dict
-        yaml_string = yaml.dump(result_dict, default_flow_style=False, sort_keys=False)
-        return yaml_string
+        return result_dict
 
     def deequ_result_desc(self, spark_df, spark):
         raise NotImplementedError("TODO")
